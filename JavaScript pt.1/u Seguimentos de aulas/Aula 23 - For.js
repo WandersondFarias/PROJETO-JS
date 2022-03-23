7051507b25d7a489b244bfa0a4259c4be754daba
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:31:28 -0300

    Create Aula 23 - For.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 23 - For.js
@@ -0,0 +1,17 @@
+const students = ['Pedro', 'Matheus', 'Kayke', 'Maju', 'Thais', 'Rafa', 'Jonatas', 'Marcus', 'Miguel', 'Juliana', 'Marcelle', 'Willian']
+
+for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i]} está presente na aula`)
+    
+}
+
+// -----------------------------------------------------------------------------------------------------------
+
+for (let index = 0; index <= 10; index++) {
+    console.log(index)
+    
+}
+
+
+
+
