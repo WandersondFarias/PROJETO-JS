adc2c5bdfca0be162015144b4727c4599c4fb2ba
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:32:40 -0300

    Create Ex 4.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 4.js
@@ -0,0 +1,5 @@
+// [ ] Faça um programa que inicie com dois nomes, e o programa imprima na tela o seguinte dado: Olá, meu nome é (nome 1) e meu partner do CodeClub é (nome2)
+const myName = "Kayke"
+const nameOfMyPartner = "Matheus"
+
+console.log(`O meu nome é ${myName} e o do meu Parceiro de Xepa é ${nameOfMyPartner}`)
