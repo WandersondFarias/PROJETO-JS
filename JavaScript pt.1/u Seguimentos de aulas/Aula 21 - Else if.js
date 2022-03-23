ea0843ceaf042375a1a640e27e91a8f0ef9a9fda
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:31:05 -0300

    Create Aula 21 - Else if.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 21 - Else if.js
@@ -0,0 +1,9 @@
+const temperatura = 36
+
+if(temperatura >= 36 && temperatura <= 39){
+    console.log("A pessoa está saudável")
+} else if (temperatura > 39) {
+    console.log("A pessoa está com febre")
+} else {
+    console.log("A pessoa está com a temperatura baixa")
+}
