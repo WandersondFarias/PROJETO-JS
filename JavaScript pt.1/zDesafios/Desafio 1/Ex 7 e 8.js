5a7512ad166cc64bfdea987a66c4d27f06fa3848
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:33:01 -0300

    Create Ex 7 e 8.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 7 e 8.js
@@ -0,0 +1,13 @@
+// [ ] Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém, os dois nomes devem estar em um Array. Ex: const array = ["João", "Maria"]. João e João, imprime true. João e Maria, imprime false.
+
+// [ ] Faça um programa que imprima na tela se um nome é diferente ao outro nome digitado. Porém, os dois nomes devem estar em um Array. Ex: const array = ["João", "Maria"]. João e João, imprime false. João e Maria, imprime true.
+
+const listName = ["Maria", "Pedrinho"]
+
+console.log(listName[0] == listName[1])
+
+// --------------------------
+
+const listName = ["Maria", "Pedrinho"]
+
+console.log(listName[0] != listName[1])
