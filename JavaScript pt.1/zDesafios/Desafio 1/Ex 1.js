26a1ab44651375671ba95420b0ede3fb5369ce65
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:32:15 -0300

    Create Ex 1.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 1.js
@@ -0,0 +1,6 @@
+// [ ] Faça um programa que SOME 2 números
+
+const numberOne = Math.floor(Math.random() * 1000)
+const numberTwo = Math.floor(Math.random() * 1000)
+const result = numberOne + numberTwo
+console.log(`O número ${numberOne} somado com ${numberTwo} é igual a ${result}`)
