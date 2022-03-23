05d8241a8dd37fab14c97aceb429fdb8ebf1b37d
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:32:31 -0300

    Create Ex 15.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 15.js
@@ -0,0 +1,4 @@
+// [ ] Crie um programa que mostra o tamanho do Array. Ex: const array = [0, 1] imprime: 2. const array = ["banana", "Maça", "laranja"] imprime: 3
+
+const array = ["banana", "Maça", "laranja", "Maça"]
+console.log(array.length)
