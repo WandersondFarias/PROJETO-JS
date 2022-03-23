69e407aeb44349c5651fd2136e27d0752ffd46f1
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:30:51 -0300

    Create Aula 20 - If else.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 20 - If else.js
@@ -0,0 +1,29 @@
+// ------------------------------------
+
+if(true){
+    console.log("Terminou de executar o meu código")
+}
+
+const age = 19
+
+if(age >= 18){
+    console.log("Ele é maior de idade")
+}
+
+// ------------------------------------
+
+if(false){
+    console.log("Terminou de executar o meu código")
+} else {
+    console.log("O código não foi executado")
+}
+
+const age = 12
+
+if(age >= 18){
+    console.log("Ele é maior de idade")
+} else {
+    console.log("Ele não é maior de idade")
+}
+
+// ------------------------------------
