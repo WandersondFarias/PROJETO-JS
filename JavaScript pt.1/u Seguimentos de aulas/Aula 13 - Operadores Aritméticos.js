790fa57d55d53409dbb8e5f261bba865306a0e5e
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:30:06 -0300

    Create Aula 13 - Operadores Aritméticos.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 13 - Operadores Aritméticos.js
@@ -0,0 +1,35 @@
+//Adição
+console.log(10 + 153)
+
+//Subtração
+console.log(627 - 251)
+
+//Multiplicação
+console.log(251 * 52)
+
+//Divisão
+console.log(156 / 3)
+
+//Resto
+console.log(146 % 3)
+
+//Incremento.1
+let myNumber = 10
+myNumber--
+
+console.log(myNumber)
+
+//Incremento.2
+let myNumber = 10
+myNumber++
+myNumber++
+
+console.log(myNumber)
+
+//Incremento.3
+let myNumber = 10
+
+console.log(++myNumber)
+
+//Elevado
+console.log(3 ** 5)
