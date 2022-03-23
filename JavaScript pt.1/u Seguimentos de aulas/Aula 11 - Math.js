0312f6b4a9019ebb67b1b1c1b7632c05014916c2
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:29:41 -0300

    Create Aula 11 - Math.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 11 - Math.js
@@ -0,0 +1,11 @@
+console.log(Math.PI)  // Mostra o valor de Pi (3,14...)
+
+console.log(Math.pow(2,3))  // Isso determina o valor elevado, ou seja, 2³
+
+console.log(Math.sqrt(25,2)) // Raiz quadrada de 25, mas pode ser cúbica, quarta, quinta e etc
+
+console.log(Math.random()) // Mostra um número aleatório entre 0 e 1
+
+console.log(Math.ceil(23.9)) // Arredondar o número para "cima". Não pode usar vírgula.
+
+console.log(Math.floor(23.7)) // Arredonda o número para "baixo". Não pode usar vírgula.
