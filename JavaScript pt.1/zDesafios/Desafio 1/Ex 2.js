bd1c3fe3160c99d1062e1d3d3765614e2e4f1e05
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:32:34 -0300

    Create Ex 2.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 2.js
@@ -0,0 +1,5 @@
+// [] Faça um programa que multiplique 2 números e o resultado adicione 10
+const numberOne = Math.floor(Math.random() * 100)
+const numberTwo = Math.floor(Math.random() * 100)
+const result = numberOne * numberTwo + 10
+console.log(`O número ${numberOne} multiplicado por ${numberTwo} e somando com 10 é igual a ${result}`)
