3230d35ad9903269488b3c8ff82d053bdd084df0
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:29:07 -0300

    Create Aula 10 - ARRAY.js

---
+++ JavaScript pt.1/Tipos de dados Aulas 6,7,8,9,10/Aula 10 - ARRAY.js
@@ -0,0 +1,33 @@
+// O array é uma coleção de um ou mais objetos, do mesmo tipo, armazenados em endereços adjacentes de memória.
+
+const number = [10, 5, 20, 67]
+
+console.log(number[0]) //Ele pegou 
+
+const person = [{
+    name: "Monkey D. Luffy",  //Primeiro usuário
+    age: 19,
+    sex: "male",
+    height: 1.74,
+    wanted: 1500000000,
+    birthday: "May 5th",
+    birthplace: "Foosha Village"
+}, {
+    name: "Roronoa Zoro",  //Segundo usuário
+    age: 21,
+    sex: "male",
+    height: 1.84,
+    wanted: 1500000000,
+    birthday: "May 5th",
+    birthplace: "Foosha Village"
+}, {
+    name: "Vinsmoke Sanji",  //Terceiro usuário
+    age: 19,
+    sex: "male",
+    height: 1.74,
+    wanted: 1500000000,
+    birthday: "May 5th",
+    birthplace: "Foosha Village"
+}]
+
+console.log(person)
