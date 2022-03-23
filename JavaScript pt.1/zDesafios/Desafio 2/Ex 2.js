eee4fd0ed15987602998a484cc12ec817377bdc2
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:33:14 -0300

    Create Ex 2.js

---
+++ JavaScript pt.1/zDesafios/Desafio 2/Ex 2.js
@@ -0,0 +1,32 @@
+// [ ] Faça um programa que verifica se uma pessoa é brasileira ou não. // DEVE SER UM OBJETO
+
+const object1 = {
+    name: "Maria",
+    nationality: "Mexican",
+    age: 12,
+    height: 1.47
+}
+
+const object2 = {
+    name: "Mariana",
+    nationality: "Brazilian",
+    age: 38,
+    height: 1.63
+}
+
+if (object1.nationality == "Brazilian") {
+    console.log(`A ${object1.name} é brasileira`)
+} else {
+    console.log(`A ${object1.name} não é brasileira`)
+}
+
+if (object2.nationality == "Brazilian") {
+    console.log(`A ${object2.name} é brasileira`)
+} else {
+    console.log(`A ${object2.name} não é brasileira`)
+}
+
+
+-->
+
+!=
