ce422a35ce5334f17b88aabfb88a660cb122492a
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:30:20 -0300

    Create Aula 15 - Operadores de Comparação.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 15 - Operadores de Comparação.js
@@ -0,0 +1,61 @@
+
+const firstNumber = 10
+const seccondNumber = 20
+
+console.log(firstNumber == seccondNumber) // Comparou só os valores
+
+//-------------------------------------
+
+const firstNumber = 10
+const seccondNumber = "10"
+
+console.log(firstNumber == seccondNumber) // Comparou só os valores
+
+//-------------------------------------
+
+const firstNumber = 10
+const seccondNumber = "10"
+
+console.log(firstNumber === seccondNumber) // Comparou tudo
+
+//-------------------------------------
+
+const firstPerson = "Maria"
+const seccondPerson = "João"
+
+console.log(firstPerson != seccondPerson) // Comparou só os valores e se são diferentes
+
+//-------------------------------------
+
+const firstPerson = "Maria"
+const thirdNumber = 22
+
+console.log(firstPerson !== thirdNumber) // Comparou tudo e se são diferentes
+
+//-------------------------------------
+
+const fourthNumber = 21
+const thirdNumber = 22
+
+console.log(fourthNumber > thirdNumber)
+
+//-------------------------------------
+
+const fourthNumber = 21
+const thirdNumber = 22
+
+console.log(fourthNumber < thirdNumber)
+
+//-------------------------------------
+
+const fourthNumber = 22
+const thirdNumber = 22
+
+console.log(fourthNumber >= thirdNumber)
+
+//-------------------------------------
+
+const fourthNumber = 21
+const thirdNumber = 22
+
+console.log(fourthNumber <= thirdNumber)
