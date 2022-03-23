eead7aeeb308b1be4f120d309dc4888360be9914
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:30:26 -0300

    Create Aula 17 - Operadores lógicos.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 17 - Operadores lógicos.js
@@ -0,0 +1,10 @@
+console.log(true && true)
+console.log(true && false)
+console.log(false && false)
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || false)
+
+console.log(!true)
+console.log(!false)
