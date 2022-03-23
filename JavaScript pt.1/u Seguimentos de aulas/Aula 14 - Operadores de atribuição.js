3d354e9c026e983f0baf987960a41ac654b80a28
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:30:14 -0300

    Create Aula 14 - Operadores de atribuição.js

---
+++ JavaScript pt.1/u Seguimentos de aulas/Aula 14 - Operadores de atribuição.js
@@ -0,0 +1,25 @@
+let firstNumber = 10
+
+firstNumber = firstNumber + 2
+// ou
+firstNumber += 2
+
+console.log(firstNumber)
+
+// --------------------------------------------------
+
+let firstNumber = 10
+firstNumber -= 2
+console.log(firstNumber)
+
+// --------------------------------------------------
+
+let firstNumber = 10
+firstNumber *= 2
+console.log(firstNumber)
+
+// --------------------------------------------------
+
+let firstNumber = 10
+firstNumber /= 2
+console.log(firstNumber)
