ae2d1638214b06430a9d1d1e1a36cc84381b0cb8
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:32:44 -0300

    Create Ex 5 e 6.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 5 e 6.js
@@ -0,0 +1,13 @@
+// [ ] Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Ex: João e João, imprime true. João e Maria, imprime false.
+
+// [ ] Faça um programa que imprima na tela se um nome é diferente ao outro nome digitado. Ex: João e João, imprime false. João e Maria, imprime true.
+
+const nameOne = "Pedro"
+const nameTwo = "Maria"
+
+console.log(nameOne === nameTwo)
+
+const nameOne = "Pedro"
+const nameTwo = "Maria"
+
+console.log(nameOne !== nameTwo)
