15e50687e87de397571f464845faa46147d03ed1
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:35:28 -0300

    Create Aula 01 - Function.js

---
+++ JavaScript pt.2/Aulas/Aula 01 - Function.js
@@ -0,0 +1,11 @@
+ // Funções ou Functions
+
+// Um conjunto de instruções que executa uma tarefa ou calcula um valor.
+
+function mySonBastiel() {
+    console.log("Meu filho se chama Bastiel e ele tem 14 anos de idade")
+}
+mySonBastiel()
+mySonBastiel()
+mySonBastiel()
+mySonBastiel()
