39145daf2a47086b9a5d20745161e8659581e7d3
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:29:27 -0300

    Create Aula 8 - OBJECT.js

---
+++ JavaScript pt.1/Tipos de dados Aulas 6,7,8,9,10/Aula 8 - OBJECT.js
@@ -0,0 +1,49 @@
+const luffy = {
+    name: "Monkey D. Luffy",
+    age: 19,
+    sex: "male",
+    height: 1.74,
+    wanted: 1500000000,
+    birthday: "May 5th",
+    birthplace: "Foosha Village"
+}
+
+console.log(luffy)
+
+// [Running] node "c:\Users\Kayke\Desktop\Aulas DevClub\JavaScript pt.1\Tipos de dados Aulas 6,7,8,9,10\tempCodeRunnerFile.js"
+// {
+//     name: 'Monkey D. Luffy',
+//     age: 19,
+//     sex: 'male',
+//     height: 1.74,
+//     wanted: 1500000000,
+//     birthday: 'May 5th',
+//     birthplace: 'Foosha Village'
+//   }
+  
+//   [Done] exited with code=0 in 0.132 seconds
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+
+
+
+
+const luffy = {
+    name: "Monkey D. Luffy",
+    age: 19,
+    sex: "male",
+    height: 1.74,
+    wanted: 1500000000,
+    birthday: "May 5th",
+    birthplace: "Foosha Village"
+}
+
+console.log(luffy.wanted)
+
+// //[Running] node "c:\Users\Kayke\Desktop\Aulas DevClub\JavaScript pt.1\Tipos de dados Aulas 6,7,8,9,10\tempCodeRunnerFile.js"
+// 1500000000
+
+// [Done] exited with code=0 in 0.133 seconds
