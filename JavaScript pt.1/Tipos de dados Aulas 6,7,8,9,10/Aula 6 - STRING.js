bbf3bc6fa9116df2b4464ced7736b7616dc5f3a4
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:29:14 -0300

    Create Aula 6 - STRING.js

---
+++ JavaScript pt.1/Tipos de dados Aulas 6,7,8,9,10/Aula 6 - STRING.js
@@ -0,0 +1,15 @@
+// O STRING é um tipo de dado do tipo caracteres alfanuméricos
+// Ele tem três formas de utilizar um string:
+
+let myFirstString = "Aqui foi a minha primeira string utilizando aspas dupla"
+let mySeccondeString = 'Aqui utilizei aspas simples'
+let myThirdString = `Aqui com crase` // Pode quebrar linha e colocar valores
+
+let text = "O meu amigo me disse uma vez assim: 'Eu gosto de macarrão com peixe' "
+let text = 'O meu professor citou: "A maior arma é o ataque", porém discordei. '
+
+let numberOfPeopleInHouse = 5
+
+let myTextWithThirdString = `Na minha casa tem ${numberOfPeopleInHouse} de pessoas no total`
+
+console.log(myTextWithThirdString)
