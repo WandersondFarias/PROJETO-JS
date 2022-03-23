19d6ac17e944a9914c264901a5c29524f72c1615
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:29:34 -0300

    Create Aula 9 - BOOLEAN.js

---
+++ JavaScript pt.1/Tipos de dados Aulas 6,7,8,9,10/Aula 9 - BOOLEAN.js
@@ -0,0 +1,12 @@
+let myNumberOne = 10
+let myNumberTwo = 10
+
+console.log(myNumberOne == myNumberTwo) //True
+
+// -------------------------------------------------------------------
+
+let myNumberOne = 10
+let myNumberTwo = 20
+
+console.log(myNumberOne == myNumberTwo) //False
+
