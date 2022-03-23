c186f58b76b6d8452fee7a2919919efabac7ebca
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:37:34 -0300

    Create Ex 4.js

---
+++ JavaScript pt.2/Desafios/Rodolfo Correção  Function/Ex 4.js
@@ -0,0 +1,11 @@
+/* 
+
+[ ] Escreva um programa onde, você chame uma função, e ela diga que ano estamos! 
+
+*/
+
+const year = () => {
+    return console.log(new Date().getFullYear())
+}
+
+year()
