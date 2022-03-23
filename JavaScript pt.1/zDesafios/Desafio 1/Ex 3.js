13e7d79b2af49b6c44d886df51687b9f66962463
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-23 18:32:37 -0300

    Create Ex 3.js

---
+++ JavaScript pt.1/zDesafios/Desafio 1/Ex 3.js
@@ -0,0 +1,4 @@
+// [ ] Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33
+const number = Math.floor(Math.random() * 100)
+const result = Math.sqrt(number)
+console.log(`A raiz quadrada do número ${number} é ${result}`)
